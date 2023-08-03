df5a46390dc1b482134575af64b54087e8a1e686
lzvckaya <lzvckaya@gmail.com>
2023-08-03 23:24:39 +0900

    add user model

---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String uid;
+  final String username;
+  final int studentId;
+  final String email;
+
+  User({
+    required this.email,
+    required this.uid,
+    required this.username,
+    required this.studentId,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "uid": uid,
+        "email": email,
+        "studentId": studentId.toString()
+      };
+
+  static User fromStore(DocumentSnapshot snap) {
+    var data = snap.data() as Map<String, dynamic>;
+
+    return User(
+        email: data['email'],
+        uid: data['uid'],
+        username: data['username'],
+        studentId: data['studentId']);
+  }
+}
